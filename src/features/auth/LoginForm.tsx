dd40244444b3ce067cf56afd5788f9dbd8926c1d
longhoang <longkai21112002@gmail.com>
2024-12-13 17:33:31 +0700

    update: auth

--- src/features/auth/LoginForm.tsx
+++ src/features/auth/LoginForm.tsx
@@ -6,8 +6,8 @@ import { Form, FormRow, Input } from "~/components/form";
 import SpinnerMini from "~/components/SpinnerMini";
 
 export default function LoginForm() {
-  const [email, setEmail] = useState("admin@example.com");
-  const [password, setPassword] = useState("password123");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { login, isLogging } = useLogin();
 
