4d7e74039b0bdd6aeb2a3c6be6870ba28099f6af
longhoang <longkai21112002@gmail.com>
2025-05-23 15:15:57 +0700

    add: link demo project

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -7,6 +7,10 @@ const StyledAppLayout = styled.div`
   display: grid;
   grid-template-columns: 2fr 8fr;
   grid-template-rows: auto 1fr;
+  height: 94vh;
+`;
+
+const StyledAppLayoutWrap = styled.div`
   height: 100vh;
 `;
 
@@ -25,18 +29,41 @@ const Container = styled.div`
   margin: 0 auto;
 `;
 
+const StyledBar = styled.div`
+  display: flex;
+  padding: 0.6rem 0.3rem;
+  background-color: #e7e555c7;
+  justify-content: center;
+`;
+
+const StyledLink = styled.a`
+  color: #0b82d1;
+  text-transform: uppercase;
+  margin-left: 0.4rem;
+`;
+
 function AppLayout() {
   return (
-    <StyledAppLayout>
-      {/* <h1>App Layout</h1> */}
-      <Header />
-      <Sidebar />
-      <Main>
-        <Container>
-          <Outlet />
-        </Container>
-      </Main>
-    </StyledAppLayout>
+    <StyledAppLayoutWrap>
+      <StyledBar>
+        <span>Please wait for a minute for server spinning up, if you want you can see the demo video </span>
+        <span>
+          <StyledLink href="https://www.youtube.com/watch?v=ZkFJMGxUw1s" target="_blank">
+            here
+          </StyledLink>
+        </span>
+      </StyledBar>
+      <StyledAppLayout>
+        {/* <h1>App Layout</h1> */}
+        <Header />
+        <Sidebar />
+        <Main>
+          <Container>
+            <Outlet />
+          </Container>
+        </Main>
+      </StyledAppLayout>
+    </StyledAppLayoutWrap>
   );
 }
 
